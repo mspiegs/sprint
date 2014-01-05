b61813be9a7639a874f97ccadd50fff9c0ded224
mspiegs <matt@mycaseinc.com>
2014-01-04 21:39:58 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sprint::Application.config.secret_key_base = 'a31c6f1587255aadec431995e5173f34d4657a2d5a470638e691c2e50059f3f54260a797c74aa68938940a74d34b9702ed67008b34ea7a725007f056b1d5d5fe'
