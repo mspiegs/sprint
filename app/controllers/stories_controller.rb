310749354b4d9ff7bcc83731150ccb744bffe76e
mspiegs <matt@mycaseinc.com>
2014-01-22 15:56:08 -0800

    fixed add story bug

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -34,6 +34,7 @@ class StoriesController < ApplicationController
   def create
     @story = Story.new(story_params)
     projectid = @story.project_id
+    logger.debug projectid
     respond_to do |format|
       if @story.save
         format.html { redirect_to project_path(projectid), notice: 'Story was successfully created.' }
